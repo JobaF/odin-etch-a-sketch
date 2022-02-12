e4eb96b92f2213eafe6a5d2001cb80c9ad4c9920
Jabo Fecht <jifecht@gmail.com>
2022-02-12 17:44:06 +0100

    Add event listeners and reset button

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 const board = document.querySelector('.board')
 
-const boardWidth = board.getBoundingClientRect().width
+const boardWidth = 800
+
+const addResetButton = () => {}
 
 const initBoard = (gridCount = 16) => {
   const elementWidth = boardWidth / gridCount + 'px'
   for (let i = 0; i < gridCount; i++) {
     const row = document.createElement('div')
-    row.setAttribute('class', 'row-' + i)
+    row.classList.add('row-' + i)
     row.style.width = boardWidth + 'px'
     row.style.height = elementWidth
     board.appendChild(row)
     for (let j = 0; j < gridCount; j++) {
       const rowElement = document.createElement('div')
-      rowElement.setAttribute('class', 'row-' + i + '-element-' + j)
+      rowElement.classList.add('clickElement', 'row-' + i + '-element-' + j)
       rowElement.style.width = elementWidth
       rowElement.style.height = elementWidth
       rowElement.style.display = 'inline-block'
-      rowElement.style.textAlign = 'center'
       row.appendChild(rowElement)
     }
   }
 }
 
-const addClickListeners = () => {}
+const addClickListeners = () => {
+  const allButtons = document.querySelectorAll('.clickElement')
+
+  allButtons.forEach((button) => {
+    button.addEventListener('mouseover', () => {
+      button.style.backgroundColor = 'black'
+    })
+  })
+}
 
 initBoard()
+addClickListeners()
