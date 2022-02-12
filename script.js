fcc13942adc694e67960aa7c43c572c3c98da5f3
Jabo Fecht <jifecht@gmail.com>
2022-02-12 17:10:13 +0100

    Add stylings to the rows and board

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const board = document.querySelector('.board')
 
+board.style.backgroundColor = 'blue'
+const boardWidth = board.getBoundingClientRect().width
+
 const initBoard = (gridCount = 16) => {
+  const elementWidth = boardWidth / gridCount + 'px'
+  console.log(elementWidth)
   for (let i = 0; i < gridCount; i++) {
     const row = document.createElement('div')
     row.setAttribute('class', 'row-' + i)
+    row.style.width = boardWidth + 'px'
+    row.style.height = elementWidth
+    row.innerHTML = 'row ' + i
     board.appendChild(row)
     for (let j = 0; j < gridCount; j++) {
       const rowElement = document.createElement('div')
