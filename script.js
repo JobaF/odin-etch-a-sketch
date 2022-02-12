8522231ce4f86a95cee181f6e0aee39bd910f252
Jabo Fecht <jifecht@gmail.com>
2022-02-12 17:21:43 +0100

    Set all elements to their respective row

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ const boardWidth = board.getBoundingClientRect().width
 
 const initBoard = (gridCount = 16) => {
   const elementWidth = boardWidth / gridCount + 'px'
-  console.log(elementWidth)
   for (let i = 0; i < gridCount; i++) {
     const row = document.createElement('div')
     row.setAttribute('class', 'row-' + i)
     row.style.width = boardWidth + 'px'
     row.style.height = elementWidth
-    row.innerHTML = 'row ' + i
     board.appendChild(row)
     for (let j = 0; j < gridCount; j++) {
       const rowElement = document.createElement('div')
       rowElement.setAttribute('class', 'row-' + i + '-element-' + j)
+      rowElement.style.width = elementWidth
+      rowElement.style.height = elementWidth
+      rowElement.style.display = 'inline-block'
+      rowElement.style.textAlign = 'center'
       row.appendChild(rowElement)
     }
   }
 }
+
+const addClickListeners = () => {}
 
 initBoard()
